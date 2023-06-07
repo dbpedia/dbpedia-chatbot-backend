d6fb12ac223f0eeae850d2896d27ffd4b1c1872e
kothari_muskan <60001051+muskan-k@users.noreply.github.com>
2023-06-07 22:56:20 +0530

    deleted dbpedia_website

--- dbpedia_website/public/chatbot.js
+++
@@ -1,8 +0,0 @@
-const dfMessenger = document.querySelector('df-messenger');
-dfMessenger.addEventListener('event-type', function (event) {
-    console.log("Chatbot loaded and initialized");
-    dfMessenger.renderCustomText('Custom text');
-    dfMessenger.addEventListener('event-type', function (event) {
-        console.log("df-list-element-clicked"+event.detail.element.title);
-    });
-});
